77ae166f689d2edb319c9debef519ac30c7d31f8
Cong Le <longchike@gmail.com>
2025-06-28 00:58:30 -0700

    1st try - lovely work

---
+++ Earthquake_Protection/Earthquake_ProtectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Earthquake_ProtectionApp.swift
+//  Earthquake_Protection
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Earthquake_ProtectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
